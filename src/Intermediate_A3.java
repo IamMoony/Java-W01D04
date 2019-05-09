667ac6495846b189f3a287b2bc289fe78fd07576
Christian Simic <christian.simicibwl@hotmail.com>
2019-05-09 10:46:25 +0200

    Intermediate A3 in work

--- src/Intermediate_A3.java
+++ src/Intermediate_A3.java
@@ -1,6 +1,21 @@
+import java.util.Scanner;
+
 public class Intermediate_A3 {
 
+    public static void main (String[] args) {
 
 
+    Scanner insert = new Scanner (System.in);
+         System.out.println("Geben Sie die erste Zahl ein: ");
+
+    int variable = insert.nextInt();
+
+         System.out.println("Geben Sie die zweite Zahl ein: ");
+
+    int varaible2 = insert.nextInt();
+
+         for( i)
+
+    }
 
 }
