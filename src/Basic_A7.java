6aef4e9fe01921524ccf7cd8e3aa47e6077c84a1
Christian Simic <christian.simicibwl@hotmail.com>
2019-05-09 13:54:02 +0200

    Intermediate A3 in work

--- src/Basic_A7.java
+++ src/Basic_A7.java
@@ -1,5 +1,12 @@
 public class Basic_A7 {
-    public static void main(String[] args){
-        int[] ints = {1, 2, 3, 4, 5, 6 ,7 ,8 ,9 , 10};
+    public static void main(String[] args) {
+        int[] ints = {1, 9, 3, 4, 5, 6 ,7 ,8 ,9 , 10};
+
+        for(int i=0; i< ints.length; i++){
+
+            System.out.println(i);
+
+        }
     }
 }
+
