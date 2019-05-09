d305b762cb8d5f25319299a4a05052ab279be66b
Simon Blaha <s.blaha@live.at>
2019-05-09 13:23:47 +0200

    upload: challenge a6

--- src/Challenge_A6.java
+++ src/Challenge_A6.java
@@ -4,5 +4,19 @@ public class Challenge_A6 {
 
     public static void main(String[] args){
 
+        int i,j,n;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter number: ");
+        n = input.nextInt();
+
+
+        for(i=1; i <= n; i++){
+            for(j=1; j<=i; j++) {
+                System.out.print(j);
+
+            }
+            System.out.println("");
+        }
+
     }
 }
