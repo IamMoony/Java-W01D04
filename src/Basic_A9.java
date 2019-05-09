c866f769d4b399e2a91afa5851a29bde2f17905d
Christian Simic <christian.simicibwl@hotmail.com>
2019-05-09 14:46:30 +0200

    Basic 9 first exercise finished

--- src/Basic_A9.java
+++ src/Basic_A9.java
@@ -1,2 +1,24 @@
 public class Basic_A9 {
+
+
+    public static void main (String[] args){
+
+        double [] temperature ={1.1,2.3,2.2,1.3,3.1};
+
+        double sum= 0;
+
+        for (int i =0; i< temperature.length; i++){
+            sum += temperature[i];
+        }
+        double average = sum/temperature.length;
+
+        System.out.println(average);
+
+
+
+
+
+
+
+    }
 }
