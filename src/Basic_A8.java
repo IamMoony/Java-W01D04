c9a392add08bd50e36af627a9cadb9fee06b0214
Benjamin Schneider <benjamin.schneider.97@gmail.com>
2019-05-09 14:26:01 +0200

    finish basic a8

--- src/Basic_A8.java
+++ src/Basic_A8.java
@@ -1,2 +1,24 @@
+import java.util.Scanner;
+
 public class Basic_A8 {
+
+    public static void main(String[] args){
+
+        Scanner input = new Scanner(System.in);
+
+        int[] mynumbers = new int[10];
+
+        for (int i = 0; i < mynumbers.length; i++){
+
+            System.out.print("Enter the " + (i+1) + ") number: ");
+            mynumbers[i] = input.nextInt();
+
+        }
+
+        for (int i = mynumbers.length-1; i >= 0; i--){
+
+            System.out.print(mynumbers[i]);
+
+        }
+    }
 }
