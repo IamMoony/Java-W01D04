11d2520ed90a92c6ea9ec21b38ade321c3221bf5
Simon Blaha <s.blaha@live.at>
2019-05-09 10:30:54 +0200

    upload: basic exercise 2

--- src/Basic_A1.java
+++ src/Basic_A1.java
@@ -1,7 +1,7 @@
 public class Basic_A1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
 
         for(int i = 1; i < 101; i++) {
             System.out.println(i);
