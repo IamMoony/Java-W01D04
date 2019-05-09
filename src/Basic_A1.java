2819232b2bd9f6a6c9a7a2e7fc4a61be1f3dabdd
Simon Blaha <s.blaha@live.at>
2019-05-09 10:25:04 +0200

    finish basic exercise 1

--- src/Basic_A1.java
+++ src/Basic_A1.java
@@ -2,5 +2,9 @@ public class Basic_A1 {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+        for(int i = 1; i < 101; i++) {
+            System.out.println(i);
+        }
     }
 }
