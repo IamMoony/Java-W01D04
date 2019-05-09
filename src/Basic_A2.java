e7353b3893e5cead3264f246bb71cf858e7c9b76
Benjamin Schneider <benjamin.schneider.97@gmail.com>
2019-05-09 10:34:56 +0200

    finish basic a2

--- src/Basic_A2.java
+++ src/Basic_A2.java
@@ -1,12 +1,9 @@
 public class Basic_A2 {
 
     public static void main(String[] args) {
-
-
-        for(int i = 2; i < 101; i++) {
-            if(i % 2 == 0) {
-                System.out.println(i);
-            }
+        
+        for (int i = 2; i < 101; i+=2) {
+            System.out.println(i);
         }
     }
 }
