375a291f9f3fa075c82e420e62f8a3a104effab1
Benjamin Schneider <benjamin.schneider.97@gmail.com>
2019-05-09 14:55:11 +0200

    finish a9 number 2 and 3

--- src/Basic_A9.java
+++ src/Basic_A9.java
@@ -1,9 +1,8 @@
 public class Basic_A9 {
 
-
     public static void main (String[] args){
 
-        double [] temperature ={1.1,2.3,2.2,1.3,3.1};
+        double [] temperature ={8.1,2.3,12.2,1.3,3.1,0.8};
 
         double sum= 0;
 
@@ -16,9 +15,28 @@ public class Basic_A9 {
 
 
 
+        double minval = temperature[0];
+
+        for (int i = 0; i < temperature.length; i++){
+
+            if (temperature[i] < minval){
+                minval = temperature[i];
+            }
+        }
+
+        System.out.println(minval);
 
 
+        double maxval = temperature[0];
 
+        for (int i = 0; i < temperature.length; i++){
+
+            if(maxval < temperature[i]){
+                maxval = temperature[i];
+            }
+        }
+
+        System.out.println(maxval);
 
     }
 }
